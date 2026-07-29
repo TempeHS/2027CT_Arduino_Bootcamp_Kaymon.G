aedf76c7ca088804156f1ce5850e07d9fb474ece
Kaymon G <kaymon.gurrala@education.nsw.gov.au>
2026-07-29 23:28:32 +0000

    feat: serial monitor experiments complete

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -31,11 +31,11 @@
 // The setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(115200);                  // Start talking to the computer at 115200 baud
-  Serial.println("Arduino ready!");    // When does this line print?
+  Serial.print("Count: ");    // When does this line print?    // When does this line print?
 }
 
 // The loop function runs over and over again forever
 void loop() {
-  Serial.println("...your own message here...");
+  Serial.print(42);
   delay(1000);
 }
